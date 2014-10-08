98fc49fe8128a8d3a389460613e361dacee4b085
Jack Deadman <jackdeadman@ymail.com>
2014-10-08 21:36:01 +0100

    add support for binary addition

--- binary.js
+++ binary.js
@@ -39,6 +39,42 @@ var Binary = (function(){
 		}
 		return remainder;
 	}
+
+	Binary.prototype.add = function(numberToAdd){
+		if (numberToAdd instanceof Binary) {
+
+			var num1 = this._digits;
+			var num2 = numberToAdd._digits;
+			var newNum = [];
+
+			var lengthOfNum1 = num1.length;
+			var lengthOfNum2 = num2.length;
+
+			var length = Math.max(lengthOfNum1, lengthOfNum2);
+
+			var num1Rev = num1.reverse();
+			var num2Rev = num2.reverse();
+
+			var carry = 0;
+			var i = 0;
+			while (carry || i < length){
+				num1 = num1Rev[i] || 0;
+				num2 = num2Rev[i] || 0;
+
+				var workingValue = num1 + num2 + carry;
+				carry = 0;
+				if (workingValue > 1) {
+					carry = 1;
+				};
+				newNum[i] = workingValue % 2;
+				i++;
+			}
+			this.setValue(newNum.reverse().join(''));
+
+		}else{
+			throw "Cannot add a non binary number";
+		}
+	};
 	
 	return Binary;
 })();
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-	// DOM Cache
+	binaryConversion();
+	binaryAddition();
+});
+
+function binaryConversion() {
 	var form = document.getElementById('binaryForm');
 	var result = document.getElementById('result');
 	var binaryValueField = document.getElementById('binaryValue');
@@ -15,4 +19,21 @@ document.addEventListener('DOMContentLoaded', function(){
 			result.innerHTML = error;
 		}
 	});
-});
+}
+
+function binaryAddition() {
+	var form = document.getElementById('additionForm');
+	var num1 = document.getElementById('num1');
+	var num2 = document.getElementById('num2');
+	var resultBox = document.getElementById('result2');
+
+	form.addEventListener('submit', function(e){
+		e.preventDefault();
+		var bin1 = new Binary(num1.value);
+		var bin2 = new Binary(num2.value);
+
+		var result = bin1.add(bin2);
+		resultBox.innerHTML = bin1.toDenary();
+		
+	});
+}
